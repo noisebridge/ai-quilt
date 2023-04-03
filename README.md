198b05ea47f8549e52668566aabcc0fa24cf0db2
tmelanson17 <tmelanson17@gmail.com>
2023-04-03 16:17:55 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ai-quilt
+
+Creating an AI-based quilt for Noisebridge.
+
+Currently only has the Arduino code, but we could add pictures for this README.
---
+++ arduino-code.ino
@@ -0,0 +1,99 @@
+#include <HCSR04.h>
+#include <VariableTimedAction.h>
+#include "FastLED.h"
+
+#define NUM_LEDS 50
+#define DISTANCE_THRESHOLD 50 // We can change this in the future.
+
+
+CRGB leds[NUM_LEDS];
+CRGB ROULETTE[] = {CRGB::Beige, CRGB::Blue, CRGB::Red, CRGB::Yellow, CRGB::Green, CRGB::DarkCyan, CRGB::Magenta, CRGB::DarkBlue, CRGB::Maroon, CRGB::MediumPurple};
+int startInteger = 0;
+size_t roulette_length = sizeof(ROULETTE)/sizeof(ROULETTE[0])-1;
+CRGB color;
+bool dim = false;
+
+unsigned char incomingBytes[4];
+unsigned char storePacket[10];
+const char HEADER[] = {'a', 'z'};
+unsigned long duration = 1000;
+unsigned long msBeatTime = 0;
+size_t colorIndex = 0;
+
+#define N_SENSORS 1
+
+struct HcResult {
+  bool objectDetected[N_SENSORS];
+};
+/*
+ * @param sensors : Object representing the sensor array
+ * @param threshold : Threshold below which an object is detected
+ * @return For each sensor, whether an object is detected there.
+ */
+HcResult detectObject(const HCSR04& sensors, float threshold) {
+  HcResult output;
+  for (int i = 0; i < N_SENSORS; ++i) {
+    float distance = sensors.dist(i);
+    output.objectDetected[i] = (
+      distance > 0 &&
+      distance < threshold
+    );
+  }
+  return output;
+}
+
+ 
+// Non-class strobe function
+size_t change_color() {
+  return random(0, roulette_length);
+}
+
+void strobe(const unsigned long currentTime, const unsigned long lastBeatMs, const unsigned long duration, size_t color) {
+    CRGB output = ROULETTE[color];
+    double fraction_of_strobe = double(currentTime - lastBeatMs) / duration;
+    uint8_t fade = min(int(fraction_of_strobe*256), 255); // Amount to fade, maximum being total darkness.
+    output.fadeToBlackBy(fade);
+    for (int i = 0; i<NUM_LEDS; ++i) {
+      leds[i] = output;
+      leds[i].fadeToBlackBy(120);
+    }
+}
+
+
+class LED : public VariableTimedAction {
+public:
+
+  //this method will be called at your specified interval
+  unsigned long run() override {
+    unsigned long currentTime = millis();  
+    // Change color if next beat.
+    while ((currentTime - msBeatTime)  > duration) {
+      colorIndex = change_color();
+      msBeatTime += duration;
+    }
+  
+    strobe(currentTime, msBeatTime, duration, colorIndex);
+  
+    FastLED.show();
+  }
+  unsigned long lastUpdate = 0;
+
+};
+
+LED led;
+HcResult sensorResults;
+HCSR04 hc(6, new int[N_SENSORS] {11}, N_SENSORS); //initialisation class HCSR04 (trig pin , echo pin, number of sensor)
+void setup() { 
+  FastLED.addLeds<NEOPIXEL, 6>(leds, NUM_LEDS); 
+  Serial.begin(115200);
+}
+
+
+void loop() {  
+  sensorResults = detectObject(hc, DISTANCE_THRESHOLD);
+  if (millis() - led.lastUpdate > 20) {
+    led.run();
+    led.lastUpdate = millis();
+  }
+}
+
